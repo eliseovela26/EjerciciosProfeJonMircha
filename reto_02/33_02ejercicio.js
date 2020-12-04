fee669250e382236a8d05f9065e494397eb7fa8f
vela lastra eliseo <eliseovela26@gmail.com>
2020-12-04 01:27:01 -0500

    Reordenando codigo

--- reto_02/33_02ejercicio.js
+++ reto_02/33_02ejercicio.js
@@ -1,13 +1,7 @@
 /* 
 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
-7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
-
 */
 
-/* const countText = (text = '', repeat = 0)=>{
-    let count = text.;
-} */
 
 const repeticiones = (texto = '', search = '')=>{
 
--- reto_02/33_03ejercicio.js
+++ reto_02/33_03ejercicio.js
@@ -1,7 +1,5 @@
 /* 
 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
-
 */
 const palindromo  = (string = '')=>{
     if(string != '')
