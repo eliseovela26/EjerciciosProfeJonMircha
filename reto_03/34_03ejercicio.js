15351eafa878920af1b919c518fcdb5ec59aa0b5
vela lastra eliseo <eliseovela26@gmail.com>
2020-12-04 11:18:47 -0500

    Ejercicios 03

--- reto_03/34_03ejercicio.js
+++ reto_03/34_03ejercicio.js
@@ -2,7 +2,6 @@
 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 */
 
-
 const factorial = (numero)=>{
     /* var result = 1;
     for (let i = 1 ; i <= numero; i++) {
